96c0fadec09a14ba53b13fbe32a47b386e9dead8
parminderkbihm <40238125+parminderkbihm@users.noreply.github.com>
2018-06-13 19:07:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var ipaddress = "http://try5-kbihm5.a3c1.starter-us-west-1.openshiftapps.com" || "127.0.0.1";
+var ipaddress = "54.174.51.64" || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var port = process.env.PORT || 1337;
