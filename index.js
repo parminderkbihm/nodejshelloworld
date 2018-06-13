f3e3109eaeccf0eddf308560bdc4d5c887403b5b
parminder_s <parminder.s@kbihm.com>
2018-06-13 18:35:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var port = process.env.PORT || 1337;
+server.listen(port, ipaddress);
+
+console.log("Server running at http://localhost:%d", port);
