8a16a646bf23fa9eb6ebc13aad11da07d428ed37
parminderkbihm <40238125+parminderkbihm@users.noreply.github.com>
2018-06-14 15:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var ipaddress = "54.174.51.64" || "127.0.0.1";
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var port = process.env.PORT || 1337;
 server.listen(port, ipaddress);
 
 console.log("Server running 2at http://localhost:%d", port);
